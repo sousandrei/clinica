c0dcba826d4c535fa301d139f8e040538e4a15ab
Andrei Sousa <andrei.s@keska.io>
2026-03-28 21:25:56 +0100

    feat: add speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { About } from './components/About';
 import { Footer } from './components/Footer';
 import { Hero } from './components/Hero';
 import { Navbar } from './components/Navbar';
 import { Services } from './components/Services';
+
+import './App.css';
 
 const App = () => {
   return (
@@ -15,6 +17,7 @@ const App = () => {
         <About />
       </main>
       <Footer />
+      <SpeedInsights />
     </div>
   );
 };
